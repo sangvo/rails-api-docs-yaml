b01ba004e3429884ce41b37b5fc8c18d6d3bb622
sangvo <sangvo111@gmail.com>
2022-08-29 14:04:07 +0700

    🐛 fix#110222: fix ticket
    - use ticket when use app ticket

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 TYPE=$(gum choose "🐛 fix" "✨ feat" "📝 docs" "💄 style" "♻️ refactor" "🧪 test" "chore" "revert")
-SCOPE=$(gum input --placeholder "scope")
+# SCOPE=$(gum input --placeholder "scope")
+TICKET=$(gum input --placeholder "ticket")
 
 # Since the scope is optional, wrap it in parentheses if it has a value.
-test -n "$SCOPE" && SCOPE="($SCOPE)"
+# test -n "$SCOPE" && SCOPE="($SCOPE)"
+# Since the scope is optional, wrap it in parentheses if it has a value.
+test -n "$TICKET" && SCOPE="($TICKET)"
 
 # Pre-populate the input with the type(scope): so that the user may change it
-SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
+# SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
+SUMMARY=$(gum input --value "$TYPE#$TICKET: " --placeholder "Summary of this change")
 DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
 
 # Commit these changes

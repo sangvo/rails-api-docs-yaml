d61d4ea7896c148c7975f70f37de00caee2ec756
sangvo <sangvo111@gmail.com>
2022-08-29 13:39:45 +0700

    feat(docs):Add post api
    - Add post api
    - Add docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts
+  # Routes for swagger docs
+  get '/swagger/*path', to: 'swagger#respond'
+  get '/swagger', to: 'swagger#respond'
 end

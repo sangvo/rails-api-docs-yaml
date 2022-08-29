3499aea85c62fadc16178fb9aba466b22640b6c4
sangvo <sangvo111@gmail.com>
2022-08-29 13:56:07 +0700

    🐛 fix: add space emoij
    - Add space emoij

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TYPE=$(gum choose "🐛fix" "✨feat" "📝docs" "💄style" "♻️refactor" "🧪test" "chore" "revert")
+TYPE=$(gum choose "🐛 fix" "✨ feat" "📝 docs" "💄 style" "♻️ refactor" "🧪 test" "chore" "revert")
 SCOPE=$(gum input --placeholder "scope")
 
 # Since the scope is optional, wrap it in parentheses if it has a value.

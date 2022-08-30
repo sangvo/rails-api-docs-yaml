59c4fcd0d8f10eb7d81370ae48d13ab59d84bc59
sangvo <sangvo111@gmail.com>
2022-08-30 16:02:40 +0700

    🐛 fix(#112): test fix
    - Detail fix
    - Issue: nope

--- bin/commit.sh
+++ bin/commit.sh
@@ -6,11 +6,11 @@ TICKET=$(gum input --placeholder "ticket")
 # Since the scope is optional, wrap it in parentheses if it has a value.
 # test -n "$SCOPE" && SCOPE="($SCOPE)"
 # Since the scope is optional, wrap it in parentheses if it has a value.
-test -n "$TICKET" && SCOPE="($TICKET)"
+test -n "$TICKET" && TICKET="(#$TICKET)"
 
 # Pre-populate the input with the type(scope): so that the user may change it
 # SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
-SUMMARY=$(gum input --value "$TYPE#$TICKET: " --placeholder "Summary of this change")
+SUMMARY=$(gum input --value "$TYPE$TICKET: " --placeholder "Summary of this change")
 DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
 
 # Commit these changes
